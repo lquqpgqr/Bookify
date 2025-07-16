7a219effdb1e605d43a1958df96ee7d32b430da9
Md. Abir Hossain <abir.user@outlook.com>
2025-07-16 19:23:31 +0600

    Introduce Domain Event pattern

--- Bookify.Domain/Abstractions/Entity.cs
+++ Bookify.Domain/Abstractions/Entity.cs
@@ -1,12 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-namespace Bookify.Domain.Abstractions;
+﻿namespace Bookify.Domain.Abstractions;
 
 public abstract class Entity
 {
+    private readonly List<IDomainEvent> _domainEvents = [];
+
     protected Entity(Guid id)
     {
         Id = id;
     }
+
     public Guid Id { get; init; }
+
+    public IReadOnlyList<IDomainEvent> GetDomainEvents()
+    {
+        return _domainEvents;
+    }
+
+    public void ClearDomainEvents()
+    {
+        _domainEvents.Clear();
+    }
+
+    protected void RaiseDomainEvent(IDomainEvent domainEvent)
+    {
+        _domainEvents.Add(domainEvent);
+    }
 }
