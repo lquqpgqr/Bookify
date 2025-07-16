d00f50260f6720d11ffa9ab021533a88d36daf6a
Md. Abir Hossain <abir.user@outlook.com>
2025-07-16 19:06:56 +0600

    Add project files.

---
+++ Bookify.Domain/Abstractions/Entity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Bookify.Domain.Abstractions;
+
+public abstract class Entity
+{
+    protected Entity(Guid id)
+    {
+        Id = id;
+    }
+    public Guid Id { get; init; }
+}
---
+++ Bookify.Domain/Apartments/Currency.cs
@@ -0,0 +1,20 @@
+﻿namespace Bookify.Domain.Apartments;
+
+public record Currency
+{
+    internal static readonly Currency None = new("");
+    public static readonly Currency Usd = new("USD");
+    public static readonly Currency Bdt = new("BDT");
+
+    private Currency(string code) => Code = code;
+
+    public string Code { get; set; }
+
+    public static Currency FromCode(string code)
+    {
+        return All.FirstOrDefault(x => x.Code == code) ??
+            throw new ApplicationException("Currency not supported by the system");
+    }
+
+    public static readonly IReadOnlyCollection<Currency> All = new[] { Usd, Bdt };
+}
