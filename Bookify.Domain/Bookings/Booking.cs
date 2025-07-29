b29e3d8f7f0bf2aeafc55024d4ceffd8ec47b51d
Md. Abir Hossain <abir.user@outlook.com>
2025-07-29 21:09:42 +0600

    update booking reserve incorporate pricing service and apertment

--- Bookify.Domain/Bookings/Booking.cs
+++ Bookify.Domain/Bookings/Booking.cs
@@ -1,4 +1,5 @@
 ﻿using Bookify.Domain.Abstractions;
+using Bookify.Domain.Apartments;
 using Bookify.Domain.Bookings.Events;
 using Bookify.Domain.Shared;
 
@@ -44,15 +45,17 @@ public sealed class Booking : Entity
     public DateTime? CancelledOnUtc { get; private set; }
 
     public static Booking Reserve(
-        Guid apartmentId,
+        Apartment apartment,
         Guid userId,
         DateRange duration,
         DateTime utcNow,
-        PricingDetails pricingDetails)
+        PricingService pricingService)
     {
+        var pricingDetails = pricingService.CalculatePrice(apartment, duration);
+
         var booking = new Booking(
             Guid.NewGuid(),
-            apartmentId,
+            apartment.Id,
             userId,
             duration,
             pricingDetails.PriceForPeriod,
@@ -62,6 +65,8 @@ public sealed class Booking : Entity
             BookingStatus.Reserved,
             utcNow
             );
+
+        apartment.LastBookedOnUtc = utcNow;
 
         booking.RaiseDomainEvent(new BookingReservedDomainEvent(booking.Id));
 
